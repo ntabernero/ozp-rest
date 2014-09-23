57ff7fd5a5b8e00349fff37a5f97309f769c9255
Rob Szewczyk <rob@gmail.com>
2014-09-23 12:32:20 -0400

    Adding application/json as a mediaType to the IWC API endpoint

--- grails-app/providers/marketplace/rest/IwcApiRepresentationWriter.groovy
+++ grails-app/providers/marketplace/rest/IwcApiRepresentationWriter.groovy
@@ -4,10 +4,14 @@ import marketplace.hal.AbstractRepresentationWriter
 import marketplace.Profile
 
 import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
 import javax.ws.rs.ext.Provider
 
 @Provider
-@Produces([IwcApiRepresentation.MEDIA_TYPE])
+@Produces([
+    IwcApiRepresentation.MEDIA_TYPE,
+    MediaType.APPLICATION_JSON
+])
 class IwcApiRepresentationWriter extends AbstractRepresentationWriter<Profile> {
     IwcApiRepresentationWriter() {
         super(new IwcApiRepresentation.Factory())
